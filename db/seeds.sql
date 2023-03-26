c8ca50798665d9b3512d9da5323546424af94d8d
Wes Harrison <wesharrison89@gmail.com>
2023-03-26 11:35:03 -0700

    started seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (id, name)
+VALUES (001,),
+       (002,),
+       (003,),
+       (004,),
+
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (101,),
+       (102,),
+       (103,),
+       (104,),
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (201,),
+       (202,),
+       (203,),
+       (204,),
+       (205,),
+       (206,),
