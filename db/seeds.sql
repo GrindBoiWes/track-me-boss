15b459722875b1c7ed51f7e8f3d4c4da2e41e3bd
Wes Harrison <wesharrison89@gmail.com>
2023-03-28 00:17:22 -0700

    debugging seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,12 @@
-USE employee_tracked;
+/*USE employee_tracked;*/
 
-INSERT INTO departments (id, name)
+INSERT INTO departments (name)
 VALUES ('Sales'),
        ('Marketing'),
        ('Engineering'),
        ('Legal');
-       
 
-
-INSERT INTO roles (id, title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ('Sales Manager', 125000, 1),
        ('Sales Representative', 75000, 1),
        ('Marketing Manager', 115000, 2),
@@ -19,7 +17,7 @@ VALUES ('Sales Manager', 125000, 1),
        ('Legal Team Lead', 250000, 4),
        ('Lawyer', 190000, 4);
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL),
        ('Mike', 'Chan', 2, 1),
        ('Ashley', 'Rodriguez', 3, NULL),
@@ -28,4 +26,3 @@ VALUES ('John', 'Doe', 1, NULL),
        ('Malia', 'Brown', 6, 5),
        ('Sarah', 'Lourd', 7, NULL),
        ('Tom', 'Allen', 8, 7);
-    
